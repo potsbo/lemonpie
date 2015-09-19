90a253cc53c270dd0de3bcf95f5ad6d3ea87bd2b
Shimpei Otsubo <potsbo@gmail.com>
2015-09-20 02:29:27 +0900

    12時間以内の任意の終日でないイベントを表示可能

--- lemonpie/ViewController.swift
+++ lemonpie/ViewController.swift
@@ -7,29 +7,113 @@
 //
 
 import UIKit
+import EventKit
 
 class ViewController: UIViewController {
+	
+	let eventStore = EKEventStore()
+	let pieClock = Pie()
+	var shorter:CGFloat {
+		get {
+			return min(screenWidth, screenHeight)
+		}
+	}
+	var screenWidth: CGFloat {
+		return self.view.bounds.width
+	}
+	var screenHeight: CGFloat {
+		return self.view.bounds.height
+	}
+	
+	override func viewWillAppear(animated: Bool) {
+		checkCalendarAuthorizationStatus()
+	}
+	let excludeAllDay = true
+	
+	
+	func checkCalendarAuthorizationStatus() {
+		let status = EKEventStore.authorizationStatusForEntityType(.Event)
+		
+		switch (status) {
+		case EKAuthorizationStatus.NotDetermined:
+			// This happens on first-run
+			requestAccessToCalendar()
+		case EKAuthorizationStatus.Authorized:
+			// Things are in line with being able to show the calendars in the table view
+			loadPie()
+			readEvents()
+//			refreshTableView()
+			break
+		case EKAuthorizationStatus.Restricted, EKAuthorizationStatus.Denied:
+			// We need to help them give us permission
+			//needPermissionView.fadeIn()
+			break
+		default:
+			let alert = UIAlertView(title: "Privacy Warning", message: "You have not granted permission for this app to access your Calendar", delegate: nil, cancelButtonTitle: "OK")
+			alert.show()
+		}
+	}
 
+	func requestAccessToCalendar() {
+		eventStore.requestAccessToEntityType(.Event, completion: handler)
+	}
+	func handler(granted: Bool, error: NSError?) {
+	// put your handler code here
+	}
+	
+	func readEvents(){
+		
+		/* Instantiate the event store */
+		let eventStore = EKEventStore()
+		
+		/* The event starts from today, right now */
+		let startDate = NSDate()
+		
+		/* The end date will be 1 day from today */
+		let endDate = startDate.dateByAddingTimeInterval(12 * 60 * 60)
+		
+		/* Create the predicate that we can later pass to the
+		event store in order to fetch the events */
+		let searchPredicate = eventStore.predicateForEventsWithStartDate(
+			startDate,
+			endDate: endDate, calendars: nil)
+		
+		/* Fetch all the events that fall between
+		the starting and the ending dates */
+		let events = eventStore.eventsMatchingPredicate(searchPredicate) as [EKEvent]
+		
+		if events.count == 0 {
+			print("No events could be found")
+		} else {
+			
+			// Go through all the events and print them to the console
+			for event in events{
+				if(excludeAllDay && event.allDay){
+					continue
+				}
+				
+				print("Event title = \(event.title)")
+				print("Event start date = \(event.startDate)")
+				print("Event end date = \(event.endDate)")
+				pieClock.addPiece(event.startDate, end: event.endDate)
+			}
+		}
+		
+	}
+	
+	func loadPie() {
+		
+		pieClock.frame = CGRectMake((screenWidth - shorter)/2, (screenHeight - shorter)/2, shorter, shorter)
+		
+		pieClock.addHourHand(NSDate())
+		
+		self.view.addSubview(pieClock)
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-		let screenWidth = self.view.bounds.width
-		let screenHeight = self.view.bounds.height
-		let shorter = min(screenWidth, screenHeight)
-
-		let pieClock = Pie(frame: CGRectMake((screenWidth - shorter)/2, (screenHeight - shorter)/2, shorter, shorter))
-		let calendar = NSCalendar(identifier: NSCalendarIdentifierGregorian)!
-		
-		let a = calendar.dateWithEra(1, year: 2015, month: 9, day: 20, hour: 1, minute: 0, second: 0, nanosecond: 0)!
-		let b = calendar.dateWithEra(1, year: 2015, month: 9, day: 20, hour: 2, minute: 0, second: 0, nanosecond: 0)!
-		let c = calendar.dateWithEra(1, year: 2015, month: 9, day: 20, hour: 9, minute: 0, second: 0, nanosecond: 0)!
-		let d = calendar.dateWithEra(1, year: 2015, month: 9, day: 20, hour: 17, minute: 0, second: 0, nanosecond: 0)!
-
-		pieClock.addPiece(a, end: b)
-		pieClock.addPiece(c, end: d)
-		pieClock.addHourHand(NSDate())
-
-		self.view.addSubview(pieClock)
+	
 	}
 
 	override func didReceiveMemoryWarning() {
