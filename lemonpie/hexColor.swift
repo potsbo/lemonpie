794f433d15a3081c3f84e113aa614f460f93fc2e
Shimpei Otsubo <potsbo@gmail.com>
2018-02-08 12:26:28 +0900

    Fix

--- lemonpie/hexColor.swift
+++ lemonpie/hexColor.swift
@@ -10,18 +10,19 @@ import Foundation
 import UIKit
 
 extension UIColor {
-	class func hexStr (var hexStr : NSString, alpha : CGFloat = 1.0) -> UIColor {
-		hexStr = hexStr.stringByReplacingOccurrencesOfString("#", withString: "")
-		let scanner = NSScanner(string: hexStr as String)
+    class func hexStr(hexStr : String, alpha : CGFloat = 1.0) -> UIColor {
+        var hexStr = hexStr
+        hexStr = hexStr.replacingOccurrences(of: "#", with: "") as String
+        let scanner = Scanner(string: hexStr as String)
 		var color: UInt32 = 0
-		if scanner.scanHexInt(&color) {
+        if scanner.scanHexInt32(&color) {
 			let r = CGFloat((color & 0xFF0000) >> 16) / 255.0
 			let g = CGFloat((color & 0x00FF00) >> 8) / 255.0
 			let b = CGFloat(color & 0x0000FF) / 255.0
 			return UIColor(red:r,green:g,blue:b,alpha:alpha)
 		} else {
 			print("invalid hex string")
-			return UIColor.whiteColor();
+            return UIColor.white;
 		}
-	}
+	 }
 }
